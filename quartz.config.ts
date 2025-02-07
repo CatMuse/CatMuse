35de4884c8b3db78730d8ddd8a4ad09792d91afa
KairosBrain <kairos.jiang@gmail.com>
2025-02-07 16:08:42 +0800

    Submit test

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "jzhao.xyz",
+    pageTitle: "🐱 CatMuse",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "jzhao.xyz",
+    baseUrl: "catmuse.me",
     ignorePatterns: ["private", "templates"],
     defaultDateType: "created",
     generateSocialImages: false,
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,8 +9,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0",
-      Twitter: "https://twitter.com/_jzhao",
+      GitHub: "https://github.com/CatMuse",
+      RSS: "https://catmuse.me/index.xml",
     },
   }),
 }
