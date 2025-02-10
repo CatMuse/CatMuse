865fdf1e55a0dc7d1eda23362451a99d5ec33d19
CatMuse <kairos.jiang@gmail.com>
2025-02-10 09:25:00 +0800

    test

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,7 +18,7 @@ export const sharedPageComponents: SharedLayout = {
         category: 'Announcements',
         // from data-category-id
         categoryId: 'DIC_kwDON1wDZs4Cmywd',
-        themeUrl: "https://catmuse.me/static/giscus", // corresponds to quartz/static/giscus/
+        themeUrl: "https://www.catmuse.me/static/giscus", // corresponds to quartz/static/giscus/
         lightTheme: "light", // corresponds to light-theme.css in quartz/static/giscus/
         darkTheme: "dark", // corresponds to dark-theme.css quartz/static/giscus/
         mapping: "pathname",
